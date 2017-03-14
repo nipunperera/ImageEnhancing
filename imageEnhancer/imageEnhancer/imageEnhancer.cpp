f5911650a0f3ce89525bd3741d720fc2d331a102
Nipun Perera <nipun.wnd@gmail.com>
2017-03-14 17:40:47 +0530

    Read and  Display Image

--- imageEnhancer/imageEnhancer/imageEnhancer.cpp
+++ imageEnhancer/imageEnhancer/imageEnhancer.cpp
@@ -1,5 +1,7 @@
-//-------------------------------DOCUMENTATION---------------------------------------
+//-------------------------------DOCUMENTATION--------------------------------------------
 //	13/03/2017 - Nipun - Opening an image within project folder and displaying
+//					   - Defining separate functions for reading and displaying the image
+//					   - White balance functionality was added
 
 
 // Include header files
@@ -7,6 +9,7 @@
 #include <opencv2/core/core.hpp>
 #include <opencv2/imgcodecs.hpp>
 #include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
 #include <iostream>
 #include <string>
 
@@ -16,8 +19,24 @@ using namespace std;
 // Image read function
 int readImage(Mat *mainImage)
 {
-	string imageName("Images\\flower.jpg"); 
-	*mainImage = imread(imageName.c_str(), IMREAD_COLOR); 
+	string imageName("Images\\flower.jpg");
+	*mainImage = imread(imageName.c_str(), IMREAD_COLOR);
+	return 0;
+}
+
+// Display image
+int displayImage(Mat *mainImage, string imageDes)
+{
+	Mat tempImage;
+	tempImage = *mainImage;
+	if (tempImage.empty())                      // Check for invalid input
+	{
+		cout << "Could not open or find the image" << endl;
+		return -1;
+	}
+	namedWindow(imageDes, WINDOW_AUTOSIZE);		// Create a window for display.
+	imshow(imageDes, tempImage);                // Show our image inside it.
+	waitKey(0);									// Wait for a keystroke in the window
 	return 0;
 }
 
@@ -26,13 +45,6 @@ int main(int argc, char** argv)
 {
 	Mat mainImage;								// Declaring the variable to store the image
 	readImage(&mainImage);						// Function call to read the image
-	if (mainImage.empty())                      // Check for invalid input
-	{
-		cout << "Could not open or find the image" << endl;
-		return -1;
-	}
-	namedWindow("Original Image", WINDOW_AUTOSIZE);		// Create a window for display.
-	imshow("Original Image", mainImage);                // Show our image inside it.
-	waitKey(0);											// Wait for a keystroke in the window
+	displayImage(&mainImage, "Original Image");
 	return 0;
 }
