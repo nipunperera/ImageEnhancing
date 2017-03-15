e7ebb049731180ca901d64892bb9b2d0b8a43038
manojMadarasingha <chamarakcm@gmail.com>
2017-03-15 22:41:44 +0530

    Configure the project for VS 2013 and start coding colorNoiseReduction

--- imageEnhancer/imageEnhancer/colorNoiseRedcution.cpp
+++ imageEnhancer/imageEnhancer/colorNoiseRedcution.cpp
@@ -1,1 +1,6 @@
-#include<sac.h>
+#include <opencv2/core/core.hpp>
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <iostream>
+#include <string>
