fc157752d4bd0ea5f4ea010888d7e5b9d744c940
dileepamarasinghe <dileepamarasinghe@y7mail.com>
2017-03-15 12:48:01 +0530

    Image cropping function added

--- imageEnhancer/imageEnhancer/imageEnhancer.cpp
+++ imageEnhancer/imageEnhancer/imageEnhancer.cpp
@@ -1,6 +1,7 @@
 //-------------------------------DOCUMENTATION---------------------------------------
 //	13/03/2017 - Nipun - Opening an image within project folder and displaying
 //  14/03/2017 - Dileepa - Added image saving function
+//	15/03/2017 - Dileepa - Image cropping function added
 
 
 // Include header files
@@ -23,7 +24,6 @@ int readImage(Mat *mainImage)
 }
 
 //Image save Function
-
 int saveImage(Mat *image, string imageName){
 
 	char  savePath[100];
@@ -31,6 +31,20 @@ int saveImage(Mat *image, string imageName){
 	imwrite( savePath, *image );
 	return 0;
 
+}
+
+//Image crop
+// @input  image - a pointer to the image to be cropped
+// @inout  cropRectangle - a pointer to a cv::Rect object which defines the boundary of the crop
+
+int imageCrop(Mat *image , Rect *cropRectangle){
+
+	Mat imageTemp= *image;
+	// Crop the full image to that image contained by the rectangle 
+	Mat croppedImage = imageTemp(*cropRectangle);
+	namedWindow("Cropped Image", WINDOW_AUTOSIZE);		  // Create a window for display.
+	imshow("Cropped Image", croppedImage);                // Show our image inside it.
+	return 0;
 }
 
 // Main function
@@ -44,10 +58,12 @@ int main(int argc, char** argv)
 		return -1;
 	}
 	
-
 	namedWindow("Original Image", WINDOW_AUTOSIZE);		// Create a window for display.
 	imshow("Original Image", mainImage);                // Show our image inside it.
 	saveImage(&mainImage, "new");
+
+	Rect tempRect(50,100, 200 ,200);   // This rectangle dimensions should be given through the mouse events
+	imageCrop(&mainImage,&tempRect);
 	waitKey(0);											// Wait for a keystroke in the window
 	return 0;
 }
