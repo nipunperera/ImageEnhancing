0034a61dca32a96fa03b90f29ea270e3588f160a
dileepamarasinghe <dileepamarasinghe@y7mail.com>
2017-03-15 21:18:16 +0530

    vignette function added

--- imageEnhancer/imageEnhancer/imageEnhancer.cpp
+++ imageEnhancer/imageEnhancer/imageEnhancer.cpp
@@ -8,6 +8,7 @@
 #include "stdafx.h"
 #include <opencv2/core/core.hpp>
 #include <opencv2/imgcodecs.hpp>
+#include <opencv2/imgproc.hpp>
 #include <opencv2/highgui/highgui.hpp>
 #include <iostream>
 #include <string>
@@ -47,6 +48,39 @@ int imageCrop(Mat *image , Rect *cropRectangle){
 	return 0;
 }
 
+//Vignette 
+int vignetteFilter(Mat *image, int sigma){
+
+	Mat imageTemp = *image;
+	vector<Mat> channels(3), filtChannel;
+	Mat filteredChannel[3];
+
+	Mat kernel_x = getGaussianKernel(imageTemp.rows, sigma);
+	Mat kernel_y = getGaussianKernel(imageTemp.cols, sigma);
+	Mat filter = kernel_x * kernel_y.t();
+	Mat mask (imageTemp.rows,imageTemp.cols, CV_8UC1);
+	normalize(filter,mask,0,255,NORM_MINMAX, CV_8UC1);
+	
+	split(imageTemp,channels);
+	
+	filteredChannel[0] = channels[0].mul(mask)/255;
+	filteredChannel[1] = channels[1].mul(mask)/255;
+	filteredChannel[2] = channels[2].mul(mask)/255;
+
+	filtChannel.push_back(filteredChannel[0]);
+	filtChannel.push_back(filteredChannel[1]);
+	filtChannel.push_back(filteredChannel[2]);
+	
+	Mat filteredImage(imageTemp.rows,imageTemp.cols, CV_8UC3);
+	merge(filtChannel,filteredImage);
+	
+	namedWindow("Filtered Image", WINDOW_AUTOSIZE);		    // Create a window for display.
+	imshow("Filtered Image", filteredImage);                // Show our image inside it.
+	return 0;
+
+}
+
+
 // Main function
 int main(int argc, char** argv)
 {
@@ -63,7 +97,8 @@ int main(int argc, char** argv)
 	saveImage(&mainImage, "new");
 
 	Rect tempRect(50,100, 200 ,200);   // This rectangle dimensions should be given through the mouse events
-	imageCrop(&mainImage,&tempRect);
+	//imageCrop(&mainImage,&tempRect);
+	vignetteFilter(&mainImage, 80);
 	waitKey(0);											// Wait for a keystroke in the window
 	return 0;
 }
