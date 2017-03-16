33f0d3f2794295c7604130b4ea4ecd52ddc0c6d7
SahaniGoonetilleke <sahani236@gmail.com>
2017-03-17 00:37:26 +0530

    GUI for contrast and exposure adjustments. Auto-adjust contrast by histogram equalization.

--- imageEnhancer/imageEnhancer/imageEnhancer.cpp
+++ imageEnhancer/imageEnhancer/imageEnhancer.cpp
@@ -21,11 +21,15 @@ using namespace std;
 /// Global variables
 Mat src, img, image;
 
-int elem1 = 255;
-int elem2 = 255;
-int elem3 = 255;
+int elem1 = 255, elem2 = 255, elem3 = 255;
+int elem4 = 255 ;
+int alpha_slider = 1;
+int bright = 50, contrast = 50;
+double alpha;
+double beta;
 
 int const max_elem = 500;
+double const max_alpha = 3;
 
 // Image read function
 int readImage(Mat *mainImage)
@@ -51,7 +55,7 @@ int displayImage(Mat *mainImage, string imageDes)
 	return 0;
 }
 
-//----------------Exposure and Contrast Adjustment---------------------//
+//----------------Exposure and Contrast Adjustment - OLD---------------------//
 // Takes an input image as source image
 int exposureContrast(Mat *mainImage)
 {
@@ -77,16 +81,16 @@ int exposureContrast(Mat *mainImage)
 	std::cout << "Enter the Brightness (beta) value (0-100): "; std::cin >> beta;
 
 	// new_image(i,j) = alpha*tempImage(i,j) + beta operation 
-	// Access each pixel of each RGB colour planes
+	// Access each pixel of each BGR colour planes
 	for (int j = 0; j < tempImage.rows; j++) {			// Loop rows
 		for (int i = 0; i < tempImage.cols; i++) {		// Loop columns
-			for (int c = 0; c < 3; c++) {				// Loop 3 RGB (0,1,2) colour planes
+			for (int c = 0; c < 3; c++) {				// Loop 3 BGR (0,1,2) colour planes
 				newImage.at<Vec3b>(j, i)[c] =
 					saturate_cast<uchar>(alpha*(tempImage.at<Vec3b>(j, i)[c]) + beta);	//Converts type 
 			}
 		}
 	}
-	// Optimized version of above
+	// Optimized version of above - used in GUI
 	//tempImage.convertTo(new_image, -1, alpha, beta);
 
 	// Create Windows
@@ -102,22 +106,61 @@ int exposureContrast(Mat *mainImage)
 
 	return 0;
 }
+//---------------------Contrast :( ----------------------------------------//
+void Contrast1(int, void *)
+{
+	int contra = elem4 - 1;
+	image = src;
+	// new_image(i,j) = alpha*tempImage(i,j) + beta operation 
+	// Access each pixel of each BGR colour planes
+	for (int y = 0; y < image.cols; y++) {			// Loop columns
+		for (int x = 0; x < image.rows; x++) {		// Loop rows
+			//for (int c = 0; c < 3; c++) {				// Loop 3 BGR (0,1,2) colour planes
+				int cur1 = image.at<Vec3b>(Point(y, x))[0];
+				int cur2 = image.at<Vec3b>(Point(y, x))[1];
+				int cur3 = image.at<Vec3b>(Point(y, x))[2];
+				cur1 *= contra;
+				cur2 *= contra;
+				cur3 *= contra;
+				if (cur1 < 0) cur1 = 0; else if (cur1 > 255) cur1 = 255;
+				if (cur2 < 0) cur2 = 0; else if (cur2 > 255) cur2 = 255;
+				if (cur3 < 0) cur3 = 0; else if (cur3 > 255) cur3 = 255;
+
+				image.at<Vec3b>(Point(y, x))[0] = cur1;
+				image.at<Vec3b>(Point(y, x))[1] = cur2;
+				image.at<Vec3b>(Point(y, x))[2] = cur3;
+			//}
+		}
+	}
+	imshow("image", image);
+}
+
+//------------------------------Histogram Equalization----------------------------//
+int HistEq()
+{
+	vector<Mat> channels;
+	cvtColor(src, image, CV_BGR2YCrCb);			// Change the color image from BGR to YCrCb format
+	split(image, channels);						// Split the image into channels
+	equalizeHist(channels[0], channels[0]);		// Equalize histogram on the 1st channel (Y)
+	merge(channels, image);						// Merge the 3 channels 
+	cvtColor(image, image, CV_YCrCb2BGR);		// Back to BGR
+	imshow("Histogram Equalized Image", image);	// Shows image
+	waitKey(0);									//wait for key press
+	return 0;
+}
 
 //----------------Saturation - Colour Space Conversion - OLD ------------------------------//
 int saturation(Mat *mainImage, string imageDes)
 {
 	Mat tempImage;
 	tempImage = *mainImage;
-
 	// Check image existance
 	if (tempImage.empty())
 	{
 		cout << "Could not open the image" << endl;
 		return -1;
 	}
-
 	Mat newImage;			
-
 	// convert BGR image to HSV
 	cvtColor(tempImage, newImage, CV_BGR2HSV);
 	namedWindow("Original image", CV_WINDOW_AUTOSIZE);		//Display Original BGR image
@@ -133,7 +176,6 @@ int saturation(Mat *mainImage, string imageDes)
 	const float* ranges[] = { h_ranges, s_ranges };
 	// Use the o-th and 1-st channels
 	int channels[] = { 0, 1 };
-
 	double sat;
 	std::cout << "Enter the Saturation value: "; std::cin >> sat;		//User input normalized sat value
 	vector<Mat> hsv_planes;
@@ -156,57 +198,33 @@ int saturation(Mat *mainImage, string imageDes)
 	imshow("Original image", tempImage);
 	namedWindow("HSV", CV_WINDOW_AUTOSIZE);
 	imshow("HSV", newImage);
-
-	Mat satBGR;
+//	Mat satBGR;
 	// convert HSV image to BGR
-	cvtColor(newImage, satBGR, CV_HSV2BGR);
-	namedWindow("BGR", CV_WINDOW_AUTOSIZE);
-	imshow("BGR", satBGR);
-
+//	cvtColor(newImage, satBGR, CV_HSV2BGR);
+//	namedWindow("BGR", CV_WINDOW_AUTOSIZE);
+//	imshow("BGR", satBGR);
 	waitKey(0);
-
 	return 0;
 }
 
-// Function Headers */
-
-void Hue(int, void*);
-void Saturation(int, void*);
-void Value(int, void*);
-void Processing();
-
-void Hue(int, void *)
+//------------------Saturation Adjustment with GUI ------------------------//
+void HSV()
 {
-	Processing();
-}
-
-void Saturation(int, void *)
-{
-	Processing();
-}
-
-void Value(int, void *)
-{
-	Processing();
-}
-
-
-void Processing()
-{
-	cvtColor(src, img, CV_RGB2HSV);		//Convert from RGB to HSV
+	cvtColor(src, img, CV_BGR2HSV);		//Convert from BGR to HSV
 
 	//Initialize
 	int hue = elem1 - 255;
 	int saturation = elem2 - 255;
 	int value = elem3 - 255;
 
+	// Loop for each pixel in each HSV plane
 	for (int y = 0; y<img.cols; y++)
 	{
 		for (int x = 0; x<img.rows; x++)
 		{
-			int cur1 = img.at<Vec3b>(Point(y, x))[0];
-			int cur2 = img.at<Vec3b>(Point(y, x))[1];
-			int cur3 = img.at<Vec3b>(Point(y, x))[2];
+			int cur1 = img.at<Vec3b>(Point(y, x))[0];			// Hue Plane
+			int cur2 = img.at<Vec3b>(Point(y, x))[1];			// Saturation Plane
+			int cur3 = img.at<Vec3b>(Point(y, x))[2];			// Value Plane
 			cur1 += hue;
 			cur2 += saturation;
 			cur3 += value;
@@ -221,28 +239,83 @@ void Processing()
 		}
 	}
 
-	cvtColor(img, image, CV_HSV2RGB);			// Convert HSV to RGB
-	imshow("image", image);						// Dispay aadjusted image
-
+	cvtColor(img, image, CV_HSV2BGR);			// Convert HSV to BGR
+	imshow("image", image);						// Dispay adjusted image
 }
+void Hue(int, void *)
+{
+	HSV();
+}
+void Saturation(int, void *)
+{
+	HSV();
+}
+void Value(int, void *)
+{
+	HSV();
+}
+void Luminance(int, void *)
+{
+	cvtColor(src, img, CV_BGR2YCrCb);			// Change the color image from BGR to YCrCb format
+
+	//Initialize
+	int luminosity = elem4 - 255;
+
+	// Loop for each pixel in each HSV plane
+	for (int y = 0; y<img.cols; y++)
+	{
+		for (int x = 0; x<img.rows; x++)
+		{
+			int cur1 = img.at<Vec3b>(Point(y, x))[0];			// Y-Luminosity Plane
+			cur1 += luminosity;
+			if (cur1 < 0) cur1 = 0; else if (cur1 > 255) cur1 = 255;
+			img.at<Vec3b>(Point(y, x))[0] = cur1;
+		}
+	}
+	cvtColor(img, image, CV_YCrCb2BGR);			// Back to BGR
+	imshow("image", image);						// Dispay adjusted image
+}
+
+// -------------------- Brightness and Contrast Adjustment with GUI----------------//
+void BC()
+{
+	Mat dst;
+	int iBrightness = bright - 50;
+	double dContrast = contrast / 25;		// Or divide by 50 to reduce range
+	src.convertTo(dst, -1, dContrast, iBrightness);
+	imshow("image", dst);
+}
+void Brightness(int, void *)
+{
+	BC();
+}
+void Contrast(int, void *)
+{
+	BC();
+}
+
 // Main function
 int main(int argc, char** argv)
 {
 	Mat mainImage;								// Declaring the variable to store the image
 	readImage(&mainImage);						// Function call to read the image
-//	displayImage(&mainImage, "Original Image");
+	displayImage(&mainImage, "Original Image");
 
+	src = mainImage;
 	//Exposure and Contrast
-	exposureContrast(&mainImage);
-
+//	exposureContrast(&mainImage);				// Old contrast Function call
+	HistEq();									//Auto-adjust Contrast ;)
 	//Saturation
 //	saturation(&mainImage, "BGR");				// Old saturation Function call
-	namedWindow("image");
+	namedWindow("image");						// Creates a window for UIs
+	imshow("image", mainImage);						// Dispay image
 	createTrackbar("Hue", "image", &elem1, max_elem, Hue);					// Slider for user to change Hue
 	createTrackbar("Saturation", "image", &elem2, max_elem, Saturation);	// Slider for user to change Saturation
-	createTrackbar("Value (Brightness)", "image", &elem3, max_elem, Value);	// Slider for user to change Value
-//	createTrackbar("", "image", &elem3, max_elem, Value);
-	src = mainImage;
+	createTrackbar("Value", "image", &elem3, max_elem, Value);	// Slider for user to change Value
+	createTrackbar("Luminance", "image", &elem4, max_elem, Luminance);	// Slider for user to change Value
+//	createTrackbar("Contrast1", "image", &elem4, max_alpha, Contrast1);
+	createTrackbar("Brightness","image",&bright, 100, Brightness);
+	createTrackbar("Contrast", "image", &contrast, 100, Contrast);
 
 	// Wait till user presses a key
 	waitKey(0);
