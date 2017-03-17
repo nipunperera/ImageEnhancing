0d7695fd814e0f3e140194ea8f15f8c3e2cca31f
Charith Dushantha <pcdushantha2@gmail.com>
2017-03-17 16:31:52 +0530

    dev-charith branch

---
+++ imageEnhancer/MyGUI/MyForm.cpp
@@ -0,0 +1,14 @@
+#include "MyForm.h"
+
+using namespace System;
+using namespace System::Windows::Forms;
+
+[STAThreadAttribute]
+
+int main(array<String^>^ args)
+{
+	Application::EnableVisualStyles();
+	Application::SetCompatibleTextRenderingDefault(false);
+	MyGUI::MyForm form;
+	Application::Run(%form);
+}
